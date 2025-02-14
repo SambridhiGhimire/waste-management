339f230712a7585abefb11a579d2f4bdce88b999
New 6969 <ghimiresambriddhi1@gmail.com>
2025-02-14 17:03:31 +0545

    Updated Database Models

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true, required: true },
-  googleId: { type: String, unique: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
-  points: { type: Number, default: 0 },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    googleId: { type: String, unique: true, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    points: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
